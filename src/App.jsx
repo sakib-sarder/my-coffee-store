7e917b2037145c1b69c8ec3aca5a45bee4c78725
sakib <s.sarder232002@gmail.com>
2023-05-08 18:44:04 +0600

    delete method added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import { useLoaderData } from 'react-router-dom'
 import './App.css'
+import CoffeeCard from './components/CoffeeCard';
+import { useState } from 'react';
 
 function App() {
+  const loadedCoffees = useLoaderData();
+  const [coffees, setCoffees] = useState(loadedCoffees);
 
   return (
-    <>
-      <h1>Vite + React</h1>
-    </>
+    <div className='container mx-auto'>
+      <h1 className='text-center my-8 text-5xl font-extrabold'>
+        Coffee: {coffees.length}
+      </h1>
+      <div className='grid md:grid-cols-2 gap-4'>
+        {
+          coffees.map(coffee=><CoffeeCard key={coffee._id} coffee={coffee} coffees={coffees} setCoffees={setCoffees}></CoffeeCard>)
+        }
+      </div>
+    </div>
   )
 }
 
--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,11 +1,66 @@
-import React from 'react';
+import React from "react";
+import { AiOutlineEye, AiOutlineEdit, AiFillDelete } from "react-icons/ai";
+import Swal from "sweetalert2";
 
-const CoffeeCard = () => {
-    return (
+const CoffeeCard = ({ coffee, coffees , setCoffees }) => {
+  const { _id, name, quantity, supplier, taste, category, details, photo } =
+    coffee;
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/coffees/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.deletedCount > 0) {
+              Swal.fire("Deleted!", "Your file has been deleted.", "success");
+              const remaining = coffees.filter((cof) => cof._id !== id);
+              setCoffees(remaining);
+            }
+          });
+      }
+    });
+  };
+  return (
+    <div className="card card-side bg-base-100 shadow-xl p-4">
+      <figure className="w-2/5">
+        <img className="w-full" src={photo} alt="Movie" />
+      </figure>
+      <div className="flex justify-between w-full">
         <div>
-            
+          <h2 className="card-title">{name}</h2>
+          <p>{supplier}</p>
+          <p>{details}</p>
         </div>
-    );
+        <div className=" my-auto">
+          <div className="btn-group space-y-2 btn-group-vertical">
+            <button className=" border py-1 rounded-lg bg-[#D2B48C]">
+              <AiOutlineEye className="text-2xl mx-auto text-white"></AiOutlineEye>
+            </button>
+            <button className=" border py-1 rounded-lg bg-[#3C393B]">
+              <AiOutlineEdit className="text-2xl mx-auto text-white"></AiOutlineEdit>
+            </button>
+            <button
+              onClick={() => handleDelete(_id)}
+              className=" border py-1 px-2 rounded-lg bg-[#EA4744]"
+            >
+              <AiFillDelete className="text-2xl mx-auto text-white"></AiFillDelete>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CoffeeCard;
